fb6c246057150cd59940f2ce19b43eacb1031fb5
Mateus Fonseca Santos <mateusfsantos.pfs@gmail.com>
2022-10-17 14:10:22 -0300

    - Music Service Created

---
+++ src/app/musics/music.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Music } from '../models/music';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MusicService {
+
+    baseURL = `${environment.MainURL}/music`;
+    
+    constructor(private http: HttpClient) { }
+
+    getAll(): Observable<Music[]> {
+
+        return this.http.get<Music[]>(`${this.baseURL}`);
+    }
+
+    getById(id: number): Observable<Music> {
+
+        return this.http.get<Music>(`${this.baseURL}/${id}`);
+    }
+
+    post(Music: Music) {
+
+        return this.http.post(`${this.baseURL}`, Music);
+    }
+
+    put(Music: Music) {
+
+        return this.http.put(`${this.baseURL}/${Music.id}`, Music);
+    }
+
+    delete(id: number) {
+
+        return this.http.delete(`${this.baseURL}/${id}`);
+    }
+}
